abb27f71b96ebaa0bb73706cad057440b56f6ea9
Abel van Hulst <abelvanhulst@gmail.com>
2026-09-02 19:27:26 +0200

    Revert "Fix NullPointerException"
    This reverts commit 769683c376ef5c9087939ffac80b36043d3d8c2c.

--- common/src/client/java/nl/abelkrijgtalles/jsmmm/RideTickHandler.java
+++ common/src/client/java/nl/abelkrijgtalles/jsmmm/RideTickHandler.java
@@ -2,17 +2,13 @@ package nl.abelkrijgtalles.jsmmm;
 
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.vehicle.boat.Boat;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MapItem;
 
 public class RideTickHandler {
     public static void handleRideTick(LocalPlayer player, HandsBusySetter handsBusySetter) {
-        Entity vehicle = player.getVehicle();
-
-        //noinspection ConditionCoveredByFurtherCondition Can cause a NullPointerException if this isn't used
-        if (vehicle == null || !(vehicle instanceof Boat)) return;
+        if (!(player.getVehicle() instanceof Boat)) return;
 
         for (InteractionHand hand : InteractionHand.values()) {
             ItemStack itemStack = player.getItemInHand(hand);
